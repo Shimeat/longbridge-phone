6c4518fd68de9ac6deb3f6942c42006413469084
Shimeat <dmitriykud-07@bk.ru>
2022-07-11 21:04:39 +0500

    fix homeswipe

--- src/components/homeswipe.js
+++ src/components/homeswipe.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 export default function HomeSwipe(props) {
   return (
     <div
-      className="w-[12.5rem] bg-[rgba(255,255,255,1)] h-1.5 rounded-2xl absolute bottom-0 left-1/4 right-0 mb-2"
+      className="w-[12.5rem] bg-[rgba(255,255,255,1)] h-1.5 rounded-2xl fixed bottom-0 right-0 left-0 mx-auto mb-2"
       {...props}
     ></div>
   );
